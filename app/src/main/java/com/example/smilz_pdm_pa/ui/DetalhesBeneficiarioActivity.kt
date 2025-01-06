3da5ccade9c84bba0039d1a2ff1e4664ddd33539
DanielVilaca <a16939@alunos.ipca.pt>
2025-01-06 19:57:07 +0000

    Limpeza Auth + Menu Escalas

--- app/src/main/java/com/example/smilz_pdm_pa/ui/DetalhesBeneficiarioActivity.kt
+++ app/src/main/java/com/example/smilz_pdm_pa/ui/DetalhesBeneficiarioActivity.kt
@@ -1,11 +1,17 @@
 package com.example.smilz_pdm_pa.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.smilz_pdm_pa.R
 import com.example.smilz_pdm_pa.databinding.ActivityDetalhesBeneficiarioBinding
 import com.example.smilz_pdm_pa.model.BeneficiarioModel
 import com.example.smilz_pdm_pa.repository.BeneficiarioRepository
+import com.google.android.material.navigation.NavigationView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -16,10 +22,67 @@ class DetalhesBeneficiarioActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetalhesBeneficiarioBinding
     private val repository = BeneficiarioRepository()
 
+    lateinit var toggle : ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetalhesBeneficiarioBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        // Configura a Toolbar personalizada
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)  // Define a toolbar como a ActionBar
+
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        //toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+
+            when(it.itemId){
+
+                //R.id.nav_home -> Toast.makeText(applicationContext, "Clicked Home", Toast.LENGTH_SHORT).show()
+                //R.id.nav_beneficiarios -> Toast.makeText(applicationContext, "Clicked Beneficiarios", Toast.LENGTH_SHORT).show()
+
+                R.id.nav_home -> {
+                    // Redireciona para a MainActivity (Beneficiarios)
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+
+                R.id.nav_beneficiarios -> {
+                    // Redireciona para a MainActivity (Beneficiarios)
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+
+                R.id.nav_calendars -> {
+                    val intent = Intent(this, EscalaActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+
+                R.id.nav_stats -> Toast.makeText(applicationContext, "Clicked Estatisticas", Toast.LENGTH_SHORT).show()
+
+                R.id.nav_logout -> {
+                    LoginActivity.logout(this)
+                }
+
+            }
+            drawerLayout.closeDrawers()
+
+            true
+        }
 
         // Configurar botão de voltar
         binding.buttonVoltar.setOnClickListener { finish() }
